d4bd498b5b3565c6a7d8387eecbc6482ed32340c
Lord-Viisaus <lord-viisaus@outlook.com>
2017-07-09 18:52:48 -0300

    Update gluster2.sh

--- gluster2.sh
+++ gluster2.sh
@@ -8,6 +8,10 @@ echo 192.168.20.11 client2 >>/etc/hosts
 echo '***** INTALANDO ******'
 echo ' '
 
+apt update
+apt install apt-transport-https
+
+
 wget -O - http://download.gluster.org/pub/gluster/glusterfs/3.7/3.7.9/pub.key | apt-key add -
 echo deb http://download.gluster.org/pub/gluster/glusterfs/3.7/3.7.9/Debian/jessie/apt jessie main > /etc/apt/sources.list.d/gluster.list
 apt-get update
